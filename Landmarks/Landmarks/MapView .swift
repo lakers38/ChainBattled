0bbd34517b5d01389e8cb70e2e5dd370ddd79a59
Praneet Kedari <praneet.kedari@gmail.com>
2022-05-21 12:26:38 -0400

    nft minting smart contract

---
+++ Landmarks/Landmarks/MapView .swift
@@ -0,0 +1,20 @@
+//
+//  MapView .swift
+//  Landmarks
+//
+//  Created by Praneet Kedari on 5/24/21.
+//
+
+import SwiftUI
+
+struct MapView_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView__Previews: PreviewProvider {
+    static var previews: some View {
+        MapView_()
+    }
+}
---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Praneet Kedari on 5/25/21.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
